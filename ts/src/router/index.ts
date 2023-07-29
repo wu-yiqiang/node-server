0f1cbeee5113cc8f0c5864678cd94818d6ccd516
wu-yiqiang <wu_yiqiang@outlook.com>
2023-07-29 19:32:43 +0800

    ww

--- ts/src/router/index.ts
+++ ts/src/router/index.ts
@@ -5,7 +5,6 @@ export const useRouter = (app:any) => {
   fs.readdirSync(__dirname).forEach((fire:any) => {
     if (fire === "index.ts") return;
     const router = require(`./${fire}`);
-    console.log('sd', fire, router)
     app.use(router.routes());
     app.use(router.allowedMethods());
   });
