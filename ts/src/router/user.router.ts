2f31ae0f61c0cd90cf995cf0684537ae74bcbe1b
wu-yiqiang <wu_yiqiang@outlook.com>
2023-07-31 22:49:00 +0800

    新增功能

--- ts/src/router/user.router.ts
+++ ts/src/router/user.router.ts
@@ -7,15 +7,15 @@ import userService from "../controller/user.controller"
 import { verifyUser, handlePassword } from "../middleware/user.middleware";
 import fs from "fs"
 import path from "path"
-//const fse = require('fs-extra')
-//const MIX_SIZE = 1024 * 1024 * 1024
-//const koaForm = require('formidable-upload-koa')
+const fse = require('fs-extra')
+const MIX_SIZE = 1024 * 1024 * 1024
+const koaForm = require('formidable-upload-koa')
 // 通过调用 router.prefix(prefix) 来设置路由的前缀
 const userRouter = new Router({ prefix: "/users" });
 const download = async (ctx:any, next:any) => {
    let fileSavePath = path.join(__dirname, "../download");
   //下载 的文件 地址
-  let fileURL = fileSavePath+"/download.msi";
+  let fileURL = fileSavePath+"/download.svg";
 
   //下载保存的文件路径
   const readStream = fs.createReadStream(fileURL);
@@ -39,41 +39,41 @@ const download = async (ctx:any, next:any) => {
 
 
 
-//async function saveFile(data:any) {
-//  const { name, type, size, filePath } = data
-//  if (size > MIX_SIZE) {
-//    await fse.remove(filePath)
-//    return Promise.reject('文件体积过大')
-//  }
-//  // 重新命名文件
-//  const fileName = Date.now() + '.' + name
-//  // 目的地
-//  const DIST_FOLDER_PATH = path.join(__dirname, '..', 'uploadFiles')
-//  const distFilePath = path.join(DIST_FOLDER_PATH, fileName)
-//  // 移动
-//  await fse.move(filePath, distFilePath)
-//
-//  // 返回
-//  return Promise.resolve({
-//    url: `http://127.0.0.1:4444/${fileName}`
+async function saveFile(data:any) {
+  const { name, type, size, filePath } = data
+  if (size > MIX_SIZE) {
+    await fse.remove(filePath)
+    return Promise.reject('文件体积过大')
+  }
+  // 重新命名文件
+  const fileName = Date.now() + '.' + name
+  // 目的地
+  const DIST_FOLDER_PATH = path.join(__dirname, '..', 'uploadFiles')
+  const distFilePath = path.join(DIST_FOLDER_PATH, fileName)
+  // 移动
+  await fse.move(filePath, distFilePath)
+
+  // 返回
+  return Promise.resolve({
+    url: `http://127.0.0.1:4444/${fileName}`
+  })
+}
+const upload = async (ctx:any, next:any) => {
+   const file = ctx.req.files['file']  // 在ctx.req.files里获取到上传的文件，['file']是前端input上传文件组件的name属性值
+  console.log('sdsda', file)
+//  const { size, path, name, type } = file  // file 里面的参数
+//  ctx.body = await saveFile({  // 将文件移动的静态资源目录，用于下载
+//    name,
+//    type,
+//    size,
+//    filePath: path
 //  })
-//}
-//const upload = async (ctx:any, next:any) => {
-//  // const file = ctx.req.files['file']  // 在ctx.req.files里获取到上传的文件，['file']是前端input上传文件组件的name属性值
-////  console.log('sd', file)
-////  const { size, path, name, type } = file  // file 里面的参数
-////  ctx.body = await saveFile({  // 将文件移动的静态资源目录，用于下载
-////    name,
-////    type,
-////    size,
-////    filePath: path
-////  })
-//}
-//const options = {
-//  uploadDir: `${__dirname}/upload/`,  // 文件存放的位置
-//  keepExtensions: true,  // 包含扩展名
-//  maxFileSize: 1024 * 1024 * 30 // 大小为 30m
-//}
+}
+const options = {
+  uploadDir: path.join(__dirname, "../upload"),  // 文件存放的位置
+  keepExtensions: true,  // 包含扩展名
+  maxFileSize: 1024 * 1024 * 30 // 大小为 30m
+}
 userRouter.post("/", verifyUser, handlePassword,userService.create);
 userRouter.post("/download", download);
 //userRouter.post("/upload", koaForm(options), upload);
