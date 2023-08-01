5e3e0f7b3f22a2ae77ed0c7a25b17444a72d7fad
wu-yiqiang <wu_yiqiang@outlook.com>
2023-08-01 22:04:45 +0800

    添加功能

--- ts/src/app/index.ts
+++ ts/src/app/index.ts
@@ -1,9 +1,23 @@
 import koa from "koa";
-import bodyParser from "koa-bodyparser";
+const cors = require('koa2-cors');
+const koaBody = require('koa-body');
 import errorHandler from "./error-handle";
 import {useRouter} from '../router';
+const fs = require('fs');
+const path = require('path');
 const app = new koa();
-app.use(bodyParser()); //解析请求参数的
+// 处理跨域
+app.use(cors({
+  //设置允许来自指定域名请求
+  origin: (ctx:any) => {
+    return '*'  // 允许来自所有域名请求
+  },
+  maxAge: 5, //指定本次预检请求的有效期，单位为秒。
+  credentials: true, //是否允许发送Cookie
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
+}));
 useRouter(app)
 app.on("error", errorHandler);
 export { app };
